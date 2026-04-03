b98ae135608729d209e20471b61fa58f841f9bdd
Robert <robert.van.der.leeuw@gmail.com>
2026-04-03 22:08:51 +0200

    Plan fleshed out.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+// TODO:
+// Collect monkey pics
+// Pick random time
+//  Between now - 4 hours
+//  Store last time in file - at least 30 hours in between.
+// On time:
+//  Open window - black screen
+//   Floating window (sway)
+//  Fade in monkey pic
+//  Drumroll
+
 fn main() {
     println!("Hello, world!");
 }
